297b4bc7561cbf644a993cf3ed3739892c51af91
Valery Borutenko <valeryborutenko@coherentsolutions.com>
2026-02-13 20:04:26 +0300

    add vite express

---
+++ src/server/server.ts
@@ -0,0 +1,10 @@
+﻿import express from 'express';
+import ViteExpress from "vite-express";
+
+const server = express();
+
+
+server.get("/message", (_, res) => res.send("Hello from express!"));
+
+
+ViteExpress.listen(server, 3001, () => console.log("Server is listening..."));
