b0a5b3f9c34ab47a97b63e3bd8f2abbb21111931
Valery Borutenko <valeryborutenko@coherentsolutions.com>
2026-02-13 21:06:42 +0300

    install dotenv

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,13 @@
 ﻿import express from 'express';
 import ViteExpress from "vite-express";
 
-const server = express();
 
+
+const server = express();
+const port = Number(process.env.PORT) || 3001;
 
 server.get("/message", (_, res) => res.send("Hello from express!"));
 
 
-ViteExpress.listen(server, 3001, () => console.log("Server is listening..."));
+
+ViteExpress.listen(server, 3001, () => console.log(`Server is listening port ${port}`));
