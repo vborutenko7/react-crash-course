1aa0d4548ed54b7f32f4d1413b0e17c402dcca14
Valery Borutenko <valeryborutenko@coherentsolutions.com>
2026-02-14 15:52:43 +0300

    rename

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,11 +3,11 @@ import ViteExpress from "vite-express";
 
 
 
-const server = express();
+const app = express();
 const port = Number(process.env.PORT) || 3001;
 
-server.get("/message", (_, res) => res.send("Hello from express!"));
+app.get("/message", (_, res) => res.send("Hello from express!"));
 
 
 
-ViteExpress.listen(server, 3001, () => console.log(`Server is listening port ${port}`));
+ViteExpress.listen(app, 3001, () => console.log(`Server is listening port ${port}`));
