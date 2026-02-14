0bba51e3a0d3971c58801c16f988bc213ec8684f
Valery Borutenko <valeryborutenko@coherentsolutions.com>
2026-02-14 21:36:27 +0300

    material template

--- src/client/shared-theme/AppTheme.tsx
+++ src/client/shared-theme/AppTheme.tsx
@@ -1,51 +1,24 @@
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import type { ThemeOptions } from '@mui/material/styles';
-import { dataDisplayCustomizations } from './customizations/dataDisplay.tsx';
-import { feedbackCustomizations } from './customizations/feedback.tsx';
-import { navigationCustomizations } from './customizations/navigation.tsx';
-import { surfacesCustomizations } from './customizations/surfaces.ts';
-import { colorSchemes, typography } from './themePrimitives.ts';
-import { inputsCustomizations } from './customizations/inputs.tsx';
 
 interface AppThemeProps {
     children: React.ReactNode;
-    disableCustomTheme?: boolean;
-    themeComponents?: ThemeOptions['components'];
 }
 
-export default function AppTheme(props: AppThemeProps) {
-    const { children, disableCustomTheme, themeComponents } = props;
-    const theme = React.useMemo(() => {
-        return disableCustomTheme
-            ? {}
-            : createTheme({
-                cssVariables: {
-                    colorSchemeSelector: 'data-mui-color-scheme',
-                    cssVarPrefix: 'template',
-                },
-                colorSchemes: {
-                    light: true,
-                    dark: true,
-                },
-                 //colorSchemes, 
-                // typography,
-                // components: {
-                //     // ...inputsCustomizations,
-                //     ...dataDisplayCustomizations,
-                //     ...feedbackCustomizations,
-                //     ...navigationCustomizations,
-                //     ...surfacesCustomizations,
-                //     ...themeComponents,
-                // },
-            });
-    }, [disableCustomTheme, themeComponents]);
-    console.log(theme);
-    if(disableCustomTheme) {
-        return <React.Fragment>{children}</React.Fragment>;
-    }
+export default function AppTheme({ children }: AppThemeProps) {
+    const customTheme = createTheme({
+        cssVariables: {
+            colorSchemeSelector: 'data-mui-color-scheme',
+            cssVarPrefix: 'template',
+        },
+        colorSchemes: {
+            light: true,
+            dark: true,
+        }
+    });
+
     return (
-        <ThemeProvider theme={theme} disableTransitionOnChange>
+        <ThemeProvider theme={customTheme} disableTransitionOnChange>
             {children}
         </ThemeProvider>
     );
