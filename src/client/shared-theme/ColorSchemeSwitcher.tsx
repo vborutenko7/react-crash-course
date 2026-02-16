f2720ae7923692371e11f8ed52e40d69bc0af0be
Valery Borutenko <valeryborutenko@coherentsolutions.com>
2026-02-16 12:38:55 +0300

    material

---
+++ src/client/shared-theme/ColorSchemeSwitcher.tsx
@@ -0,0 +1,19 @@
+﻿import { useColorScheme } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import LightModeIcon from '@mui/icons-material/LightModeRounded';
+import DarkModeIcon from '@mui/icons-material/DarkModeRounded';
+
+
+export default function ColorSchemeSwitcher() {
+    const { mode, setMode } = useColorScheme();
+    
+    const icon = mode === 'light' ?   <DarkModeIcon /> : <LightModeIcon />;
+
+    return <IconButton
+        onClick={()  => mode === 'light' ? setMode('dark') : setMode('light')}
+        size="small"
+    >
+        {icon}
+    </IconButton>
+
+}
