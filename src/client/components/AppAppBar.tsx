ed8e227595c698c94df24ab992954cf87690a55c
Valery Borutenko <valeryborutenko@coherentsolutions.com>
2026-02-17 18:23:53 +0300

    fix

--- src/client/components/AppAppBar.tsx
+++ src/client/components/AppAppBar.tsx
@@ -18,38 +18,12 @@ export default function AppAppBar() {
                     <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', px: 0 }}>
                         <Box sx={{ display: { display: 'flex' } }}>
                             <Button variant="text" color="info" size="small">
-                                Features
-                            </Button>
-                            <Button variant="text" color="info" size="small">
-                                Testimonials
-                            </Button>
-                            <Button variant="text" color="info" size="small">
-                                Highlights
-                            </Button>
-                            <Button variant="text" color="info" size="small">
-                                Pricing
-                            </Button>
-                            <Button variant="text" color="info" size="small" sx={{ minWidth: 0 }}>
-                                FAQ
-                            </Button>
-                            <Button variant="text" color="info" size="small" sx={{ minWidth: 0 }}>
-                                Blog
+                                Home work 1
                             </Button>
                         </Box>
                     </Box>
                     <Box
-                        sx={{
-                            display: 'flex',
-                            gap: 1,
-                            alignItems: 'center',
-                        }}
                     >
-                        <Button color="primary" variant="text" size="small">
-                            Sign in
-                        </Button>
-                        <Button color="primary" variant="contained" size="small">
-                            Sign up
-                        </Button>
                         <ColorSchemeSwitcher/>
                     </Box>
                 </Toolbar>
